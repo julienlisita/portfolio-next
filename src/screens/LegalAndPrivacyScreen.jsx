2b70269084ed94856efaf9e2dbda8c4ab9eed52a
Julien Lisita <julien.lisita@gmail.com>
2026-05-11 20:28:45 +0200

    migrate legal notice page and related components

--- src/screens/LegalAndPrivacyScreen.jsx
+++ src/screens/LegalAndPrivacyScreen.jsx
@@ -1,3 +1,15 @@
+// src/screens/legal/LegalAndPrivacyScreen.jsx
+
+import LegalNoticeSection from "@/components/legal/LegalNoticeSection";
+import PrivacyPolicySection from "@/components/legal/PrivacyPolicySection";
+import PageTitle from "@/components/UI/PageTitle";
+
 export default function LegalAndPrivacyScreen() {
-  return <main>Legal And Privacy Screen</main>;
+  return (
+    <>
+      <PageTitle>Mentions légales & Politique de confidentialité</PageTitle>
+      <LegalNoticeSection />
+      <PrivacyPolicySection />
+    </>
+  );
 }
